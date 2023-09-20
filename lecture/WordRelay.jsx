41e9e3248889866b7a11db6abc33a96c8ab43713
junggwanho <rhksgh1222@naver.com>
2023-09-20 14:21:07 +0900

    웹팩 설정

---
+++ lecture/WordRelay.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const { Component } = React;
+
+class WorldRelay extends Component {
+    state = {
+
+    }
+
+    render() {
+
+    }
+}
+
+module.exports = WordRelay;
--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,3 +1,20 @@
-module.exports = {
+const path = require('path');
 
-}
+module.exports = {
+    name : 'wordrelay-setting' ,
+    mode : 'development', // 실서비스: production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: ['./client'],
+    }, //입력
+
+    output: {
+        path: path.join(__dirname, 'dist'), 
+        filename: 'app.js'
+    }, // 출력
+    
+};

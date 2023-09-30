7f2054cb9a6d38ff1697b7adba300c11e2e46294
junggwanho <rhksgh1222@naver.com>
2023-09-30 20:25:31 +0900

    webpack devserver

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,30 +1,49 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
-module.exports ={
-    name: 'word-realay-setting',
-    mode: 'development', // 실서비스 : production
-    devtool: 'eval',
+module.exports = {
+    name: 'wordrelay',
+    mode: 'development', // 실서비스: production
+    devtool: 'eval', // 실서비스: hidden-source-map
     resolve: {
-        extensions: ['.js', '.jsx'],
-      },
+        extensions: ['.js', '.jsx']
+    },
 
     entry: {
         app: ['./client'],
     }, // 입력
-
     module: {
         rules: [{
-            test: /\.jsx?/,
+            test: /\.jsx?$/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties'],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 1% in KR'], // browserslist
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel',
+                ],
             },
         }],
     },
+    plugins: [
+        new RefreshWebpackPlugin()
+    ],
 
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
+        filename: 'app.js',
+        publicPath: '/dist/',
     }, // 출력
-};
+    devServer: {
+        publicPath: '/dist/',
+        hot: true
+    },
+};  
